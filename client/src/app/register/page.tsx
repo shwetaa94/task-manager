12874a4243dafcaf6fc61087d0318b782e52ac78
shweta <shwetasinghh9416@gmail.com>
2024-07-27 02:22:09 +0530

    center page

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>
+    <div className="w-full relative [background:linear-gradient(180deg,_#fff,_#afa3ff)] overflow-hidden flex flex-row items-start justify-center pt-[90px] px-5 pb-[378px] box-border leading-[normal] tracking-[normal]">
         <Register/>
     </div>
   )
